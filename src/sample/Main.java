2f0d5f999934c472ecb952b4fc17a8d44d90499f
Zhukov Nikita <nypno@mail.ru>
2022-02-12 17:16:01 +0300

    Начало чата

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,24 +1,6 @@
 package sample;
-//
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
 
 public class Main {
-
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Online Chat");
-//        primaryStage.setScene(new Scene(root, 800, 600));
-//        primaryStage.show();
-//    }
-
-
     public static void main(String[] args) {
-
-
     }
 }
